f1fd6a02b5fcb0baeb54971862143d4dbb5684a7
mim <mim3dot@gmail.com>
2022-06-19 23:29:42 -0500

    Adding a demo and test branch to install, test and showcase the project ( can be used to develop and test before (and without neededing to) publish )

---
+++ App.js
@@ -0,0 +1,68 @@
+import React, {useEffect, useState} from 'react';
+import type {Node} from 'react';
+import {StyleSheet, View} from 'react-native';
+
+import { Donut } from 'react-native-donut-chart';
+
+let initialData = [
+  {
+    value: 1,
+    color: 'brown',
+  },
+  {
+    value: 2,
+    color: 'red',
+  },
+  {
+    value: 3,
+    color: 'green',
+  },
+  {
+    value: 4,
+    color: 'gold',
+  },
+  {
+    value: 5,
+    color: 'blue',
+  },
+];
+
+const App: () => Node = () => {
+  const [data, setData] = useState(initialData);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newData = data.map(d => ({
+        ...d,
+        value: Math.round(Math.random() * 100),
+      }));
+      setData(newData);
+    }, 1999);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+          flexWrap: 'wrap',
+          alignItems: 'center',
+        }}>
+        <Donut data={data} radius={120} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
+
+export default App;
